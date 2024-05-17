821835913336b3b957f3e04d1ef79aeb3300a8fe
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-17 19:55:55 +0200

    Frontend | Modifying translation

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,21 @@
-import styled from "styled-components";
 import {Route, Routes} from "react-router-dom";
 import {logout} from "./actions/auth";
 import AuthVerify from "./common/AuthVerify";
 import {useDispatch} from "react-redux";
+import Box from "@mui/material/Box";
 // Barre de navigation
 import Navbar from "./components/Navbar";
 // Footer
 import Footer from "./components/Footer";
 // Pages
 import Home from "./pages/Home";
-import Filtres from "./pages/Filtres";
+import Categories from "./pages/Categories";
 import Category from "./pages/Category";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-`;
 
 function App() {
 
@@ -32,11 +25,16 @@ function App() {
     };
 
     return (
-        <Container>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
             <Navbar/>
             <Routes>
                 <Route path="/" exact element={<Home/>}/>
-                <Route path="/filtres" element={<Filtres/>}/>
+                <Route path="/categories" element={<Categories/>}/>
                 <Route path="/category/:categoryId" element={<Category/>}/>
                 <Route path="/product/:productId" element={<Product/>}/>
                 <Route path="/login" element={<Login/>}/>
@@ -46,7 +44,7 @@ function App() {
             </Routes>
             <AuthVerify logOut={logOut}/>
             <Footer/>
-        </Container>
+        </Box>
     );
 }
 

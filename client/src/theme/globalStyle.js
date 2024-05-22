90487bc2ad832fa5423323f3c1c5045b8ac922c7
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-22 20:38:20 +0200

    chore: Update .gitignore and package-lock.json

--- client/src/theme/globalStyle.js
+++ client/src/theme/globalStyle.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 // Fonts
 import Modernist from "./fonts/Sk-Modernist-Regular.otf";
 import Integral from "./fonts/Fontspring-DEMO-integralcf-regular.otf";
@@ -7,42 +7,54 @@ import LemonMilk from "./fonts/LEMONMILK-Regular.otf";
 import LemonMilkLight from "./fonts/LEMONMILK-Light.otf";
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: "Modernist";
-    src: url(${Modernist}) format('truetype');
-  }
+    @font-face {
+        font-family: "Modernist";
+        src: url(${Modernist}) format('truetype');
+    }
 
-  @font-face {
-    font-family: "Integral";
-    src: url(${Integral}) format('truetype');
-  }
+    @font-face {
+        font-family: "Integral";
+        src: url(${Integral}) format('truetype');
+    }
 
-  @font-face {
-    font-family: "Integral Oblique";
-    src: url(${IntegralOblique}) format('truetype');
-  }
+    @font-face {
+        font-family: "Integral Oblique";
+        src: url(${IntegralOblique}) format('truetype');
+    }
 
-  @font-face {
-    font-family: "Lemon Milk";
-    src: url(${LemonMilk}) format('truetype');
-  }
+    @font-face {
+        font-family: "Lemon Milk";
+        src: url(${LemonMilk}) format('truetype');
+    }
 
-  @font-face {
-    font-family: "Lemon Milk Light";
-    src: url(${LemonMilkLight}) format('truetype');
-  }
+    @font-face {
+        font-family: "Lemon Milk Light";
+        src: url(${LemonMilkLight}) format('truetype');
+    }
 
-  * {
-    margin: 0;
-    padding: 0;
-    font-family: "Inter", sans-serif;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Inter", sans-serif;
+    }
 
-  html, body, #root {
-    width: 100%;
-    height: 100%;
-    background-color: #121212;
-  }
+    html, body, #root {
+        width: 100%;
+        height: 100%;
+        background-color: #121212;
+    }
+
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #root {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+    }
 `;
 
 export default GlobalStyle;

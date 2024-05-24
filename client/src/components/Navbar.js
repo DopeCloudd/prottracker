bba906f9cff2b7636402fd3b09fa3e6af2261586
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-24 19:47:23 +0200

    chore: Update Navbar component with MUI icons and improve code formatting

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,116 +1,108 @@
-import React, {useEffect} from "react";
-import {useNavigate} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useGetUserDetailsQuery } from "../auth/auth.service";
+import { logout, setCredentials } from "../auth/auth.slice";
 import SwitchLangage from "./SwitchLangage";
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import LogoutIcon from '@mui/icons-material/Logout';
-import {Avatar, Box, Typography} from "@mui/material";
-import {useGetUserDetailsQuery} from "../auth/auth.service";
-import {setCredentials, logout} from "../auth/auth.slice";
 
 function Navbar() {
-    // Hooks
-    const {userInfo, userToken} = useSelector((state) => state.auth)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  // Hooks
+  const { userInfo, userToken } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    // automatically authenticate user if token is found
-    const userDetailsQuery = useGetUserDetailsQuery('userDetails', {
-        pollingInterval: 900000, // 15mins
-    });
+  // automatically authenticate user if token is found
+  const userDetailsQuery = useGetUserDetailsQuery("userDetails", {
+    pollingInterval: 900000, // 15mins
+  });
 
-    useEffect(() => {
-        if (userToken && userDetailsQuery.data) {
-            dispatch(setCredentials(userDetailsQuery.data))
-        }
-    }, [userToken, userDetailsQuery.data, dispatch])
+  useEffect(() => {
+    if (userToken && userDetailsQuery.data) {
+      dispatch(setCredentials(userDetailsQuery.data));
+    }
+  }, [userToken, userDetailsQuery.data, dispatch]);
 
-    // Event handlers for navigation button clicks
-    const handleButtonClick = () => {
-        navigate("/");
-    };
+  // Event handlers for navigation button clicks
+  const handleButtonClick = () => {
+    navigate("/");
+  };
 
-    const handleLogin = () => {
-        navigate("/login");
-    };
+  const handleLogin = () => {
+    navigate("/login");
+  };
 
-    const handleProfile = () => {
-        navigate("/profile");
-    };
+  const handleProfile = () => {
+    navigate("/profile");
+  };
 
-    return (
-        <Box
-            sx={{
-                minHeight: "100px",
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                color: "#EAEDED",
-                fontWeight: "bold",
-                fontSize: "18px",
-                padding: "0 5%",
-                "& svg": {
-                    fill: "#00A656",
-                    paddingTop: "4px",
-                },
-                "& img": {
-                    height: "100px",
-                    width: "100px",
-                    cursor: "pointer",
-                },
-            }}
-        >
-            <Typography
-                variant="h6"
-                onClick={handleButtonClick}
-                sx={{
-                    fontFamily: "Integral Oblique, sans-serif",
-                    textTransform: "uppercase",
-                    cursor: "pointer",
-                    fontSize: "clamp(1.625rem, 1.3571rem + 0.7143vw, 2rem)",
-                    "& span": {
-                        color: "#00A656",
-                        fontFamily: "Integral Oblique, sans-serif",
-                        textTransform: "uppercase",
-                    },
-                }}
-            >
-                MY<span>PROT</span>TRACKER
-            </Typography>
-            <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    "& svg": {
-                        fontSize: "38px",
-                        cursor: "pointer",
-                        padding: "0",
-                    },
-                }}
-            >
-                <SwitchLangage/>
-                {userInfo ? (
-                    <>
-                        <Box ml={2} mr={3}>
-                            <Avatar
-                                sx={{cursor: "pointer"}}
-                                onClick={handleProfile}
-                            >
-                                {userInfo.firstName
-                                    ? userInfo.firstName.substring(0, 1)
-                                    : ""}
-                            </Avatar>
-                        </Box>
-                        <LogoutIcon onClick={() => dispatch(logout())}/>
-                    </>
-                ) : (
-                    <PersonOutlineOutlinedIcon onClick={handleLogin}/>
-                )}
+  return (
+    <Box
+      sx={{
+        minHeight: "100px",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        color: "#EAEDED",
+        fontWeight: "bold",
+        fontSize: "18px",
+        padding: "0 5%",
+        "& img": {
+          height: "100px",
+          width: "100px",
+          cursor: "pointer",
+        },
+      }}
+    >
+      <Typography
+        variant="h6"
+        onClick={handleButtonClick}
+        sx={{
+          fontFamily: "Integral Oblique, sans-serif",
+          textTransform: "uppercase",
+          cursor: "pointer",
+          fontSize: "clamp(1.625rem, 1.3571rem + 0.7143vw, 2rem)",
+          "& span": {
+            color: "#00A656",
+            fontFamily: "Integral Oblique, sans-serif",
+            textTransform: "uppercase",
+          },
+        }}
+      >
+        MY<span>PROT</span>TRACKER
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SwitchLangage />
+        {userInfo ? (
+          <>
+            <Box mr={3}>
+              <Avatar sx={{ cursor: "pointer" }} onClick={handleProfile}>
+                {userInfo.firstName ? userInfo.firstName.substring(0, 1) : ""}
+              </Avatar>
             </Box>
-        </Box>
-    );
+            <Button variant="outlined" onClick={() => dispatch(logout())}>
+              <LogoutIcon fontSize="small" sx={{ mr: 1 }} />
+              <span>Logout</span>
+            </Button>
+          </>
+        ) : (
+          <Button variant="outlined" onClick={handleLogin}>
+            <LoginIcon fontSize="small" sx={{ mr: 1 }} />
+            <span>Login</span>
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
 }
 
 export default Navbar;

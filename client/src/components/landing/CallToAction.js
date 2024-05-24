564d4780e15b9015afd5803ece5efd049009deb4
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-24 19:46:25 +0200

    chore: Add CallToAction component for landing page

---
+++ client/src/components/landing/CallToAction.js
@@ -0,0 +1,44 @@
+import { Box, Button, alpha } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+const ActionButton = styled(Button)(({ theme }) => ({
+  width: "40%",
+  padding: "0.6em 2em",
+  borderRadius: "18px",
+  outline: "2px solid",
+  outlineColor: alpha("#00A656", 0.1),
+  boxShadow: `0 0 24px 12px ${alpha("#00A656", 0.2)}`,
+}));
+
+export default function CallToAction() {
+  // For translation
+  const { t } = useTranslation();
+  // For navigation
+  const navigate = useNavigate();
+  // Go to filtres page
+  const handleButtonClick = () => {
+    navigate("/categories");
+  };
+
+  return (
+    <Box
+      id="calltoaction"
+      sx={{
+        pt: 4,
+        pb: 4,
+        backgroundColor: "#171717",
+        height: "fit-content",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <ActionButton onClick={handleButtonClick}>
+        {t("home.button")}
+      </ActionButton>
+    </Box>
+  );
+}

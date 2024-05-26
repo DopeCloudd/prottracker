e82268233da9fd7a8a4c89aada80de70dddec2a8
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-26 22:19:22 +0200

    chore: Update product URLs and code formatting for multiple scrapers and pages

--- server/scraping/myprotein/myprotein_scraper.js
+++ server/scraping/myprotein/myprotein_scraper.js
@@ -1,175 +1,107 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const myprotein_pages = require('./myprotein_pages');
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const myprotein_pages = require("./myprotein_pages");
 
-puppeteer.use(StealthPlugin());
+// Helper functions to extract data from the page
+const extractText = (selector, $) => $(selector).html().trim();
+const extractPrice = (selector, $) => {
+  const priceText = $(selector).text().trim();
+  const price = priceText.replace(/[^\d,.]/g, "");
+  return parseFloat(price.replace(",", "."));
+};
+const extractImageUrl = (selector, $) => $(selector).attr("src");
+const extractQuantity = (selector, $) => {
+  const element = $(selector);
+  return element.length
+    ? element
+        .contents()
+        .filter(function () {
+          return this.type === "text";
+        })
+        .first()
+        .text()
+        .trim()
+    : null;
+};
 
-async function myproteinScraping() {
-
-    const buttonCookies = '#onetrust-accept-btn-handler';
-
-    try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            userDataDir: './puppeteerProfile'
-        });
-        const scrapePage = async (url, buttonSelector, priceSelector, titleSelector, descriptionSelector, quantitySelector, selectValue, additionalData) => {
-            const page = await browser.newPage();
-
-            await page.setRequestInterception(true);
-            page.on('request', (req) => {
-                if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
-                    req.abort();
-                } else {
-                    req.continue();
-                }
-            });
-
-            await page.setViewport({width: 1080, height: 1024});
-            await page.goto(url);
-
-            try {
-                await page.waitForSelector(buttonCookies, {timeout: 1500});
-                await page.click(buttonCookies);
-            } catch (e) {
-                console.log('No cookies');
-            }
-
-            try {
-                const ariaExpandedValue = await page.evaluate(() => {
-                    const button = document.querySelector('#product-description-heading-lg-2');
-                    return button ? button.getAttribute('aria-expanded') : null;
-                });
-                if (ariaExpandedValue === 'false') {
-                    const button = await page.$('#product-description-heading-lg-2');
-                    await button.click();
-                }
-                await page.waitForSelector(descriptionSelector, {visible: true});
-            } catch (e) {
-                console.log(e);
-            }
-
-            // Extract data
-            const data = await page.evaluate((priceSel, titleSel, descSel, quantitySel) => {
-                const extractPrice = (selector) => {
-                    const element = document.querySelector(selector);
-                    if (!element) {
-                        return null;
-                    }
-                    // Extract the text and trim it
-                    let priceText = element.innerText.trim();
-
-                    // Remove currency symbol
-                    priceText = priceText.replace('€', '').trim();
-
-                    // Replace comma with dot if it's used as a decimal separator
-                    priceText = priceText.replace(',', '.');
-
-                    return parseFloat(priceText);
-                };
-                const extractText = (selector) => {
-                    const element = document.querySelector(selector);
-                    return element ? element.innerText.trim() : null;
-                };
-
-                const extractQuantity = (selector) => {
-                    const element = document.querySelector(selector);
-                    if (!element) return null;
-                    return element.childNodes[0].nodeValue.trim();
-                };
-
-                return {
-                    price: extractPrice(priceSel),
-                    title: extractText(titleSel),
-                    description: extractText(descSel),
-                    quantity: extractQuantity(quantitySel)
-                };
-            }, priceSelector, titleSelector, descriptionSelector, quantitySelector);
-
-            await page.close();
-            // Combine the scraped data with the additionalData
-            return {...data, ...additionalData, url: url};
-        };
-
-        // Define selectors for each product
-        const selectors = {
-            Whey: {
-                buttonSelector: "div[aria-label='Quantité'] li.athenaProductVariations_listItem button[data-option-id='5859']",
-                priceSelector: "p[data-product-price='price']",
-                titleSelector: "h1[data-product-name='title']",
-                descriptionSelector: "#product-description-content-lg-2 p",
-                quantitySelector: "div[aria-label='Quantité'] li.athenaProductVariations_listItem button[data-option-id='5859']",
-                additionalData: {
-                    image: './myprotein/image/myprotein_whey.png',
-                    id_category: 1,
-                    brand: 'Myprotein'
-                }
-            },
-            Pure_Whey_Isolate: {
-                buttonSelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                priceSelector: "p[data-product-price='price']",
-                titleSelector: "h1[data-product-name='title']",
-                descriptionSelector: "#product-description-content-lg-2 p",
-                quantitySelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                additionalData: {
-                    image: './myprotein/image/myprotein_isolate.png',
-                    id_category: 1,
-                    brand: 'Myprotein'
-                }
-            },
-            Clear_Whey_Isolate: {
-                buttonSelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                priceSelector: "p[data-product-price='price']",
-                titleSelector: "h1[data-product-name='title']",
-                descriptionSelector: "#product-description-content-lg-2 p",
-                quantitySelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                additionalData: {
-                    image: './myprotein/image/myprotein_clear.png',
-                    id_category: 2,
-                    brand: 'Myprotein'
-                }
-            },
-            Gainer_Prise_De_Masse: {
-                buttonSelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                priceSelector: "p[data-product-price='price']",
-                titleSelector: "h1[data-product-name='title']",
-                descriptionSelector: "#product-description-content-lg-2 p",
-                quantitySelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                additionalData: {
-                    image: './myprotein/image/myprotein_gainer.png',
-                    id_category: 3,
-                    brand: 'Myprotein'
-                }
-            },
-            Pre_Workout: {
-                buttonSelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                priceSelector: "p[data-product-price='price']",
-                titleSelector: "h1[data-product-name='title']",
-                descriptionSelector: "#product-description-content-lg-2 p",
-                quantitySelector: "button.athenaProductVariations_box.default.athenaProductVariationsOption[data-selected]",
-                additionalData: {
-                    image: './myprotein/image/myprotein_alpha.png',
-                    id_category: 5,
-                    brand: 'Myprotein'
-                }
-            },
-        };
-
-        // Concurrently scrape all pages
-        const pagesData = await Promise.all(
-            Object.entries(myprotein_pages).map(([key, url]) => {
-                const sels = selectors[key];
-                return scrapePage(url, sels.buttonSelector, sels.priceSelector, sels.titleSelector, sels.descriptionSelector, sels.quantitySelector, sels.selectValue, sels.additionalData);
-            })
-        );
-
-        await browser.close();
-        return pagesData;
-
-    } catch (error) {
-        console.error("Error during scraping:", error.message);
+async function fetchAndExtract(page, pageInfo) {
+  // Enable request interception
+  await page.setRequestInterception(true);
+  page.on("request", (req) => {
+    if (["image", "stylesheet", "font"].includes(req.resourceType())) {
+      req.abort();
+    } else {
+      req.continue();
     }
+  });
+
+  // Navigate to the URL
+  await page.goto(pageInfo.url, { waitUntil: "networkidle2" });
+
+  // Get the HTML content of the page
+  const content = await page.content();
+
+  // Load the HTML content into cheerio
+  const $ = cheerio.load(content);
+
+  // Extract the title
+  const title = extractText("h1.productName_title", $);
+
+  // Extract the price
+  const price = extractPrice("p.productPrice_price", $);
+
+  // Extract the quantity
+  const quantity = extractQuantity(
+    "button.athenaProductVariations_box[data-selected]",
+    $
+  );
+
+  // Extract the description
+  const description = extractText(
+    "div.productDescription_contentProperties p",
+    $
+  );
+
+  // Extract the image URL
+  const imageUrl = extractImageUrl("img.athenaProductImageCarousel_image", $);
+
+  // Set the brand
+  const brand = "Myprotein";
+
+  // Set the URL
+  const url = pageInfo.url;
+
+  // Set the category id
+  const categoryId = pageInfo.id_category;
+
+  return {
+    url,
+    title,
+    price,
+    quantity,
+    description,
+    brand,
+    imageUrl,
+    categoryId,
+  };
 }
 
-// Exporting the function, not its result
-module.exports = myproteinScraping;
+async function myprotein() {
+  // Launch Puppeteer browser
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  });
+  const promises = myprotein_pages.map(async (pageInfo) => {
+    const page = await browser.newPage();
+    const data = await fetchAndExtract(page, pageInfo);
+    await page.close();
+    return data;
+  });
+  const results = await Promise.all(promises);
+  await browser.close();
+  return results;
+}
+
+module.exports = myprotein;

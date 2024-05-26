3f83671d4c5251fa289a6d8f210e29a16931612e
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-26 22:19:14 +0200

    chore: Update myprotein_pages.js with new product URLs and code formatting

--- server/scraping/myprotein/myprotein_pages.js
+++ server/scraping/myprotein/myprotein_pages.js
@@ -1,9 +1,30 @@
-const myprotein_pages = {
-    Whey: "https://fr.myprotein.com/nutrition-sportive/impact-whey-protein/10530943.html",
-    Pure_Whey_Isolate: "https://fr.myprotein.com/nutrition-sportive/impact-whey-isolate/10530911.html",
-    Clear_Whey_Isolate: "https://fr.myprotein.com/nutrition-sportive/clear-whey-isolate/12081395.html",
-    Gainer_Prise_De_Masse: "https://fr.myprotein.com/nutrition-sportive/gainer-prise-de-masse/10529988.html",
-    Pre_Workout: "https://fr.myprotein.com/nutrition-sportive/alpha-pre-workout/12941037.html",
-}
+const categories = require("../product.category");
+
+const myprotein_pages = [
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/impact-whey-protein/10530943.html",
+    id_category: categories.Whey,
+  },
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/impact-whey-isolate/10530911.html",
+    id_category: categories.Whey,
+  },
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/clear-whey-isolate/12081395.html",
+    id_category: categories.ClearWhey,
+  },
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/gainer-prise-de-masse/10529988.html",
+    id_category: categories.Gainer,
+  },
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/creapure-monohydrate-de-creatine/10529740.html",
+    id_category: categories.Creatine,
+  },
+  {
+    url: "https://fr.myprotein.com/nutrition-sportive/alpha-pre-workout/12941037.html",
+    id_category: categories.PreWorkout,
+  },
+];
 
 module.exports = myprotein_pages;

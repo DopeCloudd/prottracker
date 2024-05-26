e9afe298cf7eafa115655a452342ba651291b13e
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-26 22:19:47 +0200

    chore: Update product model with new data type for image field

--- server/db/models/product.model.js
+++ server/db/models/product.model.js
@@ -1,51 +1,51 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 module.exports = (sequelize, Sequelize) => {
-    const Product = sequelize.define("products", {
-        id_category: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        image: {
-            type: DataTypes.BLOB('long'),
-            allowNull: true
-        },
-        currentPrice: {
-            type: DataTypes.FLOAT,
-            allowNull: false
-        },
-        priceHistory: {
-            type: DataTypes.JSON,
-            allowNull: true
-        },
-        lowestPrice: {
-            type: DataTypes.FLOAT,
-            allowNull: true
-        },
-        highestPrice: {
-            type: DataTypes.FLOAT,
-            allowNull: true
-        },
-        quantity: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        url: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        brand: {
-            type: DataTypes.STRING,
-            allowNull: true
-        }
-    });
+  const Product = sequelize.define("products", {
+    id_category: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    currentPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    priceHistory: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    lowestPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    highestPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    quantity: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
 
-    return Product;
-}
+  return Product;
+};

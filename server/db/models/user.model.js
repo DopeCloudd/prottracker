03ad27a8424b1fd3482e167c124f26282d488286
Valentin Lerouge <cloudd2vi@gmail.com>
2024-05-29 21:24:17 +0200

    chore: Add stripeSessionId and paidSubscription fields to User model

--- server/db/models/user.model.js
+++ server/db/models/user.model.js
@@ -1,32 +1,41 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("users", {
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        firstName: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        email: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        emailToken: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-        emailVerification: {
-            type: Sequelize.DATE,
-            defaultValue: false,
-            allowNull: false
-        },
-        password: {
-            type: Sequelize.STRING,
-            allowNull: false
-        }
-    });
+  const User = sequelize.define("users", {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    emailToken: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    emailVerification: {
+      type: Sequelize.DATE,
+      defaultValue: false,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    stripeSessionId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    paidSubscription: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
 
-    return User;
+  return User;
 };

2e98906a52dcb505a1380c5ffb1ac94cc1f4e216
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-03 21:18:47 +0200

    chore: Migrate auth redux files to redux folder

---
+++ client/src/redux/auth/auth.actions.js
@@ -0,0 +1,58 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API = "http://localhost:3032";
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async ({ name, firstName, email, password }, { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      await axios.post(
+        `${API}/auth/signup`,
+        { name, firstName, email, password },
+        config
+      );
+    } catch (error) {
+      // return custom error message from backend if present
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, { rejectWithValue }, thunkAPI) => {
+    try {
+      // configure header's Content-Type as JSON
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${API}/auth/signin`,
+        { email, password },
+        config
+      );
+      // store user's token in local storage
+      localStorage.setItem("userToken", data.userToken);
+      return data;
+    } catch (error) {
+      // return custom error message from API if any
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);

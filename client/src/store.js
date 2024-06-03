2038fda77a229da60858d69641599ae0983577f7
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-03 21:19:39 +0200

    chore: Update store.js to include userProductsReducer in the reducer configuration

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authReducer from './auth/auth.slice'
-import {authApi} from './auth/auth.service'
+import { configureStore } from "@reduxjs/toolkit";
+import { authApi } from "./redux/auth/auth.service";
+import authReducer from "./redux/auth/auth.slice";
+import userProductsReducer from "./redux/user/user_products.slice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        [authApi.reducerPath]: authApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware),
-})
-export default store
+  reducer: {
+    userProducts: userProductsReducer,
+    auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
+});
+export default store;

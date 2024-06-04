58b637be62a56090dbd5af9d5686010ecb63e75b
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-04 23:40:39 +0200

    chore: Update BuyButton component to include required props and adjust button width based on type

--- client/src/components/product/BuyButton.jsx
+++ client/src/components/product/BuyButton.jsx
@@ -2,7 +2,19 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default function BuyButton({ url }) {
+export default function BuyButton({ url, type }) {
+  if (url === undefined) {
+    throw new Error(
+      "La prop 'url' est obligatoire pour le composant BuyButton"
+    );
+  }
+
+  if (type === undefined) {
+    throw new Error(
+      "La prop 'type' est obligatoire pour le composant BuyButton"
+    );
+  }
+
   const { t } = useTranslation();
   // Function that sends to merchant site url
   const goToUrl = (url) => {
@@ -14,11 +26,12 @@ export default function BuyButton({ url }) {
       onClick={() => goToUrl(url)}
       variant="outlined"
       sx={{
-        width: "100%",
+        width: type === "product" ? "100%" : "40%",
         borderRadius: "6px",
         fontWeight: "bold",
-        mt: 2,
-        mb: 2,
+        mt: type === "product" ? 2 : 0,
+        mb: type === "product" ? 2 : 0,
+        mr: type === "product" ? 0 : 2,
       }}
     >
       {t("product.button")}

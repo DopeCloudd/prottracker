c6aed2b12c0e9f6a04e4790d568cdbde1a3d7acc
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-04 23:40:44 +0200

    chore: Update Price component to handle different types and throw error for missing value prop

--- client/src/components/product/Price.jsx
+++ client/src/components/product/Price.jsx
@@ -1,7 +1,11 @@
 import { Typography } from "@mui/material";
 import React from "react";
 
-export default function Price({ value }) {
+export default function Price({ value, type }) {
+  if (value === undefined) {
+    throw new Error("La prop 'value' est obligatoire");
+  }
+
   const prixFormate = value.toLocaleString("fr-FR", {
     style: "currency",
     currency: "EUR",
@@ -10,11 +14,15 @@ export default function Price({ value }) {
 
   return (
     <Typography
-      component="h2"
+      component={type === "product" ? "h2" : "h4"}
       sx={{
-        mr: 1,
+        mr: type === "product" ? 1 : undefined,
         color: "#e00034",
-        fontSize: "clamp(1.375rem, 0.9063rem + 1.25vw, 2rem)",
+        fontWeight: "bold",
+        fontSize:
+          type === "product"
+            ? "clamp(1.375rem, 0.9063rem + 1.25vw, 2rem)"
+            : undefined,
       }}
     >
       {prixFormate}

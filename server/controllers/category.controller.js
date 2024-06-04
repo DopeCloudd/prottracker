d4418a28fe4d92846acfffcbca10739679472017
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-04 23:41:55 +0200

    chore: Update category controller to improve category retrieval and add category by ID functionality

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -2,13 +2,30 @@
 const db = require("../db/models");
 
 exports.getCategories = (req, res) => {
-    // Récupère tout les produits
-    db.category.findAll()
-        .then(categories =>{
-            // On renvoie la liste des categories
-            res.status(200).send(categories);
-        })
-        .catch(error => {
-            res.status(500).send({ message: error.message });
-        })
+  // Get all categories
+  db.category
+    .findAll()
+    .then((categories) => {
+      // Send the categories to the client
+      res.status(200).send(categories);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message });
+    });
+};
+
+exports.getCategoryById = (req, res) => {
+  // Get the category id
+  const { categoryId } = req.params;
+
+  // Get the category by id
+  db.category
+    .findByPk(categoryId)
+    .then((category) => {
+      // Send the category to the client
+      res.status(200).send(category);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message });
+    });
 };
